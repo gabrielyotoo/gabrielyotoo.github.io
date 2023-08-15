160d101b24bcf6b91977c8ee15abaa9e76701971
Gabriel Freitas Yamamoto <gabrielyamamoto82@gmail.com>
2023-08-15 20:06:36 -0300

    feat(socials): add icons

--- app/components/socials.tsx
+++ app/components/socials.tsx
@@ -1,11 +1,48 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Socials = () => (
   <div className="flex gap-4">
-    <Link href="#">Instagram</Link>
-    <Link href="#">GitHub</Link>
-    <Link href="#">Linkedin</Link>
-    <Link href="#">Email</Link>
+    <span className="flex">
+      <Image
+        className="mr-2"
+        alt="Instagram Icon"
+        src="/instagram.svg"
+        width={20}
+        height={20}
+      />
+      <Link href="#">Instagram</Link>
+    </span>
+    <span className="flex">
+      <Image
+        className="mr-2"
+        alt="GitHub Icon"
+        src="/github.svg"
+        width={20}
+        height={20}
+      />
+      <Link href="#">GitHub</Link>
+    </span>
+    <span className="flex">
+      <Image
+        className="mr-2"
+        alt="Linkedin Icon"
+        src="/linkedin.svg"
+        width={20}
+        height={20}
+      />
+      <Link href="#">Linkedin</Link>
+    </span>
+    <span className="flex">
+      <Image
+        className="mr-2"
+        alt="Email Icon"
+        src="/email.svg"
+        width={20}
+        height={20}
+      />
+      <Link href="#">Email</Link>
+    </span>
   </div>
 );
 
